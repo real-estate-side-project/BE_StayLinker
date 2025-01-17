54664f7d8a9777f1693c7523eb0ec2a3c74ecaaf
Ppajingae <wkmkyj991112@gmail.com>
2025-01-17 10:14:57 +0900

    ✨ feat Implement InternalServerError

--- src/main/kotlin/com/yoong/sunnyside/infra/security/CustomInternalServerErrorHandler.kt
+++ src/main/kotlin/com/yoong/sunnyside/infra/security/CustomInternalServerErrorHandler.kt
@@ -1,7 +1,9 @@
 package com.yoong.sunnyside.infra.security
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.yoong.sunnyside.common.dto.ErrorResponse
 import com.yoong.sunnyside.common.exception.InternalServerException
+import com.yoong.sunnyside.common.exception.handler.GlobalExceptionHandler
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
@@ -19,7 +21,7 @@ class CustomInternalServerErrorHandler: OncePerRequestFilter() {
             response.status = HttpServletResponse.SC_INTERNAL_SERVER_ERROR
             response.contentType = "application/json"
             val objectMapper = ObjectMapper()
-            val jsonString = objectMapper.writeValueAsString(InternalServerException("Internal Server Error"))
+            val jsonString = objectMapper.writeValueAsString(ErrorResponse(500, "Internal Server Error"))
             response.writer.write(jsonString)
         }
     }
